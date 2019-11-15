7151c1817a90c5abbb385a8501f8efa7d2c6e4c1
Dilawar Mahmood <dilawarm@stud.ntnu.no>
2019-11-15 19:29:11 +0100

    trist

---
+++ server/tests/kommentardaoTests.js
@@ -0,0 +1,37 @@
+// @flow
+
+let mysql = require("mysql");
+
+const Kommentardao = require("../src/DAO/kommentardao.js");
+const runsqlfile = require("../src/DAO/runsqlfile.js");
+
+let pool = mysql.createPool({
+    connectionLimit: 1,
+    host: "mysql",
+    user: "root",
+    password: "secret",
+    database: "supertestdb",
+    debug: false,
+    multipleStatements: true
+});
+
+let kommentardao = new Kommentardao(pool);
+
+beforeAll(done => {
+  runsqlfile("create_tables.sql", pool, () => {
+    runsqlfile("create_testdata.sql", pool, done);
+  });
+});
+
+test("get all comments from db", done => {
+  function callback(status, data) {
+    console.log(
+      "Test callback: status=" + status + ", data=" + JSON.stringify(data)
+    );
+    expect(data.length).toBe(1);
+    expect(data[0].kommentar).toBe("kommentar");
+    done();
+  }
+
+  kommentardao.getAll("Nyheter", 1, callback);
+}, 30000);
