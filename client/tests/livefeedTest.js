0df2faa6cfcd8d9eaf52318abe8991f75da54c7b
Dilawar Mahmood <dilawarm@stud.ntnu.no>
2019-11-18 14:08:44 +0100

    hei

---
+++ client/tests/livefeedTest.js
@@ -0,0 +1,25 @@
+// @flow
+
+import * as React from 'react';
+import { Component } from 'react-simplified';
+import {LiveFeed, LiveFeedElement} from '../src/Components/staticComponents';
+import { shallow, mount } from 'enzyme';
+
+describe('Livefeed tests', () => {
+    const wrapper = shallow(<LiveFeed />);
+
+    it('initially', () => {
+        let instance = LiveFeed.instance();
+        expect(typeof instance).toEqual('object');
+        if (instance) expect(instance.saker).toEqual([]);
+    });
+
+    it('after load', () => {
+
+        setTimeout(() => {
+            let instance = LiveFeed.instane();
+            expect(typeof instance).toEqual('object');
+            if (instance) except(instance.saker).toEqual([]);
+        });
+    });
+});
